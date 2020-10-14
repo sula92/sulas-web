d8ffdec9cadaf1cc252d9593de22aa0bd51c0b30
sula92 <sulakkanaid@gmail.com>
2020-10-14 18:41:35 +0530

    first

---
+++ myscript2.js
@@ -0,0 +1,49 @@
+$(".hide1").hide();
+
+function func() {
+    $("#btn1").show();
+}
+
+function func2() {
+    $("#btn1").hide();
+}
+
+function func3() {
+    $("#btn2").show();
+}
+
+function func4() {
+    $("#btn2").hide();
+}
+
+function func5() {
+    $("#btn4").show();
+}
+
+function func6() {
+    $("#btn4").hide();
+}
+
+function func7() {
+    $("#btn5").show();
+}
+
+function func8() {
+    $("#btn5").hide();
+}
+
+function func9() {
+    $("#btn6").show();
+}
+
+function func10() {
+    $("#btn6").hide();
+}
+
+function func11() {
+    $("#btn8").show();
+}
+
+function func12() {
+    $("#btn8").hide();
+}
